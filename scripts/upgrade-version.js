cdcedf5ea2ba821df0829e52fca6b643099a0a08
Ezequiel <ezequiel@leites.dev>
2020-08-30 16:46:19 +0200

    ci: fixing

--- scripts/upgrade-version.js
+++ scripts/upgrade-version.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
+const ora = require('ora');
+const chalk = require('chalk');
 
 const packageJson = require('../package.json');
 const packageJsonFile = path.join(__dirname, '../package.json');
@@ -68,31 +70,49 @@ const getNewVersion = () => {
 };
 
 const updatePackageJson = ({ version }) => {
-  console.log(`This should update to v${version}`);
+  const spinner = ora(
+    `Updating ${chalk.blue('package.json')} to ${chalk.red(`v${version}`)}.`,
+  ).start();
 
   packageJson.version = version;
-
   fs.writeFileSync(packageJsonFile, JSON.stringify(packageJson, null, 2));
+
+  spinner.succeed(`${chalk.blue('package.json')} updated.`);
 };
 
 const commitVersioning = (newVersion) => {
+  const spinner = ora('Running git status').start();
   execSync('git status', { stdio: 'inherit' });
+  spinner.succeed();
 
-  execSync(
-    'git config user.email ezequiel@leites.dev && git config user.name Ezequiel Leites',
-  );
+  spinner.start(ora('Configuring git user')).start();
+  execSync('git config user.email ezequiel@leites.dev');
+  execSync('git config user.name Ezequiel Leites');
+  spinner.succeed();
 
-  execSync(
-    `git add . && git commit -m "Release v${branchPrefix}${newVersion}" && git push origin master`,
-    { stdio: 'inherit' },
-  );
+  spinner.start(ora('Add files')).start();
+  execSync('git add .');
+  spinner.succeed('Files added');
+
+  spinner.start(ora('Commit files')).start();
+  execSync(`git commit -m "Release v${branchPrefix}${newVersion}"`);
+  spinner.succeed('Commit created');
+
+  spinner.start(ora('Push branch')).start();
+  execSync('git push origin master');
+  spinner.succeed('Branch pushed');
 };
 
 const commitTag = (newVersion) => {
+  const spinner = ora('Create new tag').start();
   execSync(
-    `git tag v${branchPrefix}${newVersion} -m "Release v${branchPrefix}${newVersion}" && git push origin v${branchPrefix}${newVersion}`,
-    { stdio: 'inherit' },
+    `git tag v${branchPrefix}${newVersion} -m "Release v${branchPrefix}${newVersion}"`,
   );
+  spinner.succeed('Tag created');
+
+  spinner.start(ora('Push tag')).start();
+  execSync(`git push origin v${branchPrefix}${newVersion}`);
+  spinner.succeed('Tag pushed');
 };
 
 // Get new version
